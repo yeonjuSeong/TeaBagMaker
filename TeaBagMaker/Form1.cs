fec379c91d66a455a66c0afc6630a819c795fe1c
YeonJu Seong <tjdduswn7933@gmail.com>
2019-05-24 11:23:56 +0900

    #2 코드 구현. 콤보박스와 텍스트 박스에 콤보박스에서 선택한 항목을 나타낼 수 있게 구현.

--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -11,24 +11,35 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        MessageBoxButtons mbb; // 메시지 버튼 옵션 설정
+        MessageBoxIcon mbi; // 메시지 버튼 아이콘 설정
+        String[] SList = new string[] { "홍차(2분)", "녹차(3분)", "루이보스차(5분)", "국화차(2분)" };
+        String orgStr = ""; // 선택 결과 저장
+
+        private void Tea_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox tea = (ComboBox)sender;
+            textBox.Text = (string)tea.SelectedItem;
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            for(int i=0; i< SList.Length;i++)
+            {
+                this.tea.Items.Add(SList[i]);
+            }
+            // this.orgStr= // 여기서 메시지 박스로
         }
+
 
         private void Label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Tea_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
